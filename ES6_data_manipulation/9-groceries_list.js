ff3981608c065acbc9d62cf75947deade1222b51
NickelannG <8836@holbertonstudents.com>
2024-07-13 21:25:33 +1000

    Task 9: Map data structure

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,11 @@
+// returns a map of groceries with items (name, quantity)
+export default function groceriesList() {
+  const groceryMap = new Map();
+  groceryMap.set('Apples', 10);
+  groceryMap.set('Tomatoes', 10);
+  groceryMap.set('Pasta', 1);
+  groceryMap.set('Rice', 1);
+  groceryMap.set('Banana', 5);
+
+  return groceryMap;
+}
