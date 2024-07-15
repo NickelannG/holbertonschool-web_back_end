f93215cfb1ca20892b9cf0dab4ccfd4ce43c62ae
NickelannG <8836@holbertonstudents.com>
2024-07-15 19:01:28 +1000

    Task 5 fix: correct output

--- ES6_promise/5-photo-reject.js
+++ ES6_promise/5-photo-reject.js
@@ -1,5 +1,5 @@
 export default function uploadPhoto(fileName) {
-  const rejectPromise = new Promise((reject) => {
+  const rejectPromise = new Promise((resolve, reject) => {
     reject(new Error(`${fileName} cannot be processed`));
   });
   return rejectPromise;
