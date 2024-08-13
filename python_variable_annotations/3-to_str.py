9262b98ad390cd06b5367a21acac3382c0a1e0cd
NickelannG <8836@holbertonstudents.com>
2024-08-13 12:26:15 +1000

    annotated return

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -2,6 +2,6 @@
 """This module contains the to_str function"""
 
 
-def to_str(n: float):
+def to_str(n: float) -> str:
     """Returns the string representation of a float number"""
     return (str(n))
