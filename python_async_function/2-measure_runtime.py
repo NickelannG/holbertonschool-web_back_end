30099f4aa943a63c99300e30038637ddfd24d8fb
NickelannG <8836@holbertonstudents.com>
2024-08-14 14:07:24 +1000

    removed async def

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -5,14 +5,14 @@ import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
-  """
-  Measure the total execution time for the imported
-  wait_n function and returns the total time
-  """
-  start_time = time.time()
-  await wait_n(n, max_delay)
-  end_time = time.time()
-  total_time = end_time - start_time
+def measure_time(n: int, max_delay: int) -> float:
+    """
+    Measure the total execution time for the imported
+    wait_n function and returns the total time
+    """
+    start_time = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end_time = time.time()
+    total_time = end_time - start_time
 
-  return total_time / n
+    return total_time / n
