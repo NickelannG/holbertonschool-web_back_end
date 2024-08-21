6e1f003fdd347f4f789562d31e37707a070b3f7e
NickelannG <8836@holbertonstudents.com>
2024-08-21 11:21:11 +1000

    file cannot be imported

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -2,19 +2,20 @@
 """Provides stats about Nginx logs sotred in MongoDB"""
 from pymongo import MongoClient
 
-client = MongoClient()
-db = client.logs
-collection = db.nginx
+if __name__ == "__main__":
+    client = MongoClient("mongodb://localhost:27017/")
+    db = client.logs
+    collection = db.nginx
 
-x = db.nginx.count_documents({})
-print("{} logs".format(x))
+    x = db.nginx.count_documents({})
+    print("{} logs".format(x))
 
 
-print("Methods:")
-list = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-for i in list:
-    db.nginx.count_documents({"i": i})
-    print(" method {}: {}".format(i, x))
+    print("Methods:")
+    list = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for i in list:
+        db.nginx.count_documents({"i": i})
+        print(" method {}: {}".format(i, x))
 
-status = db.nginx.count_documents({"method": "GET", "path": "/status"})
-print("{} satus check".format(status))
+    status = db.nginx.count_documents({"method": "GET", "path": "/status"})
+    print("{} satus check".format(status))
