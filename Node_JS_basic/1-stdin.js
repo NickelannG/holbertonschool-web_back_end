a98620244709e599b2b71c396109940196584806
NickelannG <nicoleanngorospe@gmail.com>
2024-09-05 10:07:23 +1000

    task 1 fix

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,3 +1,16 @@
+// const readline = require('readline').createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+
+// readline.question('Welcome to Holberton School, what is your name?\r', name => {
+//   console.log(`Your name is: ${name}!`);
+// });
+
+// readline.on('close', () => {
+//   console.log('This important software is now closing');
+// });
+
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
