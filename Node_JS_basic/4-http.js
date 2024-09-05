6f8f9a145e96fc88022d28542f2ae199e12677e5
NickelannG <nicoleanngorospe@gmail.com>
2024-09-05 11:07:08 +1000

    task 4 - lint fixes

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,13 +1,15 @@
-const http = require("http");
+const http = require('http');
 
 const host = 'localhost';
 const port = 1245;
 
 const requestListener = function (req, res) {
   res.writeHead(200);
-  res.end("Hello Holberton School!");
+  res.end('Hello Holberton School!');
 };
 
 const app = http.createServer(requestListener);
 app.listen(port, host, () => {
 });
+
+module.exports = requestListener;
